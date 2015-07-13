c4e5d7ae5edf8a72c4e319f6fd2e712b4208716d
Adrián Pino <adrianpinoalcalde@gmail.com>
2015-07-13 11:07:46 +0200

    Update update.py

--- update.py
+++ update.py
@@ -55,23 +55,10 @@ if len(paths)==17:
 # Date
 ystday = date.today()- timedelta(1)
 
-# Paths to status
-status_top_pp      = status_top[0][0:len(status_top[0])-1] + " #DataPolitica_TopPP"
-status_top_podemos = status_top[1][0:len(status_top[1])-1] + " #DataPolitica_TopPodemos"
-status_top_psoe    = status_top[2][0:len(status_top[2])-1] + " #DataPolitica_TopPSOE"
-status_top_cs      = status_top[3][0:len(status_top[3])-1] + " #DataPolitica_TopCs"
-status_top_iu      = status_top[4][0:len(status_top[4])-1] + " #DataPolitica_TopIU"
-status_top_upyd    = status_top[5][0:len(status_top[5])-1] + " #DataPolitica_TopUPyD"
-
-
-
 # Status
 status_all= "Menciones por partidos: " + '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_resumen"
-
 status_time = "Menciones por horas : " + '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_dia"
-
 status_mean = "Promedio tweets por usuario: " + '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_media"
-
 status_week = "Resumen de la semana política en twitter #DataPolitica_semana"
 
 status_wc_pp = "Qué dice la gente sobre el #PP: " + '%s/%s/%s'% ( ystday.day, ystday.month, ystday.year)
@@ -81,12 +68,20 @@ status_wc_cs = "Qué dice la gente sobre #Ciudadanos: "+'%s/%s/%s' % ( ystday.da
 status_wc_iu = "Qué dice la gente sobre #IU: " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
 status_wc_upyd = "Qué dice la gente sobre #UPyD: " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
 
-status_top_pp = "Top 10 tuiteros del #PP #DataPolitica_TopPP " + '%s/%s/%s'% ( ystday.day, ystday.month, ystday.year)
-status_top_podemos ="Top 10 tuiteros de #Podemos #DataPolitica_TopPodemos "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_top_psoe = "Top 10 tuiteros del #PSOE #DataPolitica_TopPSOE "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_top_cs = "Top 10 tuiteros de #Ciudadanos #DataPolitica_TopCs "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_top_iu = "Top 10 tuiteros de #IU #DataPolitica_TopIU " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_top_upyd ="Top 10 tuiteros de #UPyD #DataPolitica_TopUPyD " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
+# Paths to status
+status_top_pp      = status_top[0][0:len(status_top[0])-1] + " #DataPolitica_TopPP"
+status_top_podemos = status_top[1][0:len(status_top[1])-1] + " #DataPolitica_TopPodemos"
+status_top_psoe    = status_top[2][0:len(status_top[2])-1] + " #DataPolitica_TopPSOE"
+status_top_cs      = status_top[3][0:len(status_top[3])-1] + " #DataPolitica_TopCs"
+status_top_iu      = status_top[4][0:len(status_top[4])-1] + " #DataPolitica_TopIU"
+status_top_upyd    = status_top[5][0:len(status_top[5])-1] + " #DataPolitica_TopUPyD"
+
+#status_top_pp = "Top 10 tuiteros del #PP #DataPolitica_TopPP " + '%s/%s/%s'% ( ystday.day, ystday.month, ystday.year)
+#status_top_podemos ="Top 10 tuiteros de #Podemos #DataPolitica_TopPodemos "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
+#status_top_psoe = "Top 10 tuiteros del #PSOE #DataPolitica_TopPSOE "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
+#status_top_cs = "Top 10 tuiteros de #Ciudadanos #DataPolitica_TopCs "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
+#status_top_iu = "Top 10 tuiteros de #IU #DataPolitica_TopIU " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
+#status_top_upyd ="Top 10 tuiteros de #UPyD #DataPolitica_TopUPyD " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
 
 status_inter = "Interacciones entre tweets " +'%s/%s/%s'% ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_Interacciones"
 
