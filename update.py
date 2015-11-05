715c8e1b0084586162ae2e85bb8e55dbee86ca59
Adrián Pino <adrianpinoalcalde@gmail.com>
2015-11-05 18:14:14 +0100

    Style changes + top concepts in updates

--- update.py
+++ update.py
@@ -18,13 +18,17 @@ auth = tweepy.OAuthHandler(cons_key, cons_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-
-f=open("file_to_upload")
-paths = f.readlines()
+# Paths to files to upload
+f1 = open("file_to_upload")
+paths = f1.readlines()
 
 # Statuses of top tweeters
 f2 = open("status_top")
 status_top = f2.readlines()
+
+# Statuses of top concepts
+f3=open("status_concepts_top")
+status_concepts_top = f3.readlines()
 
 # Remove EOL in paths (last char)
 path_all         = paths[0][0:len(paths[0])-1]
@@ -50,23 +54,25 @@ path_inter       = paths[15][0:len(paths[15])-1]
 
 # Case of week summary
 if len(paths)==17:
-	path_week       = paths[16][0:len(paths[16])-1]
+	path_week = paths[16][0:len(paths[16])-1]
 
 # Date
 ystday = date.today()- timedelta(1)
+date_ystday = '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
 
 # Status
-status_all= "Menciones por partidos: " + '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_resumen"
-status_time = "Menciones por horas : " + '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_dia"
-status_mean = "Promedio tweets por usuario: " + '%s/%s/%s' % ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_media"
+status_all= "Menciones por partidos: " + date_ystday + " #DataPolitica_resumen"
+status_time = "Menciones por horas: " + date_ystday + " #DataPolitica_dia"
+status_mean = "Promedio tweets por usuario: " + date_ystday + " #DataPolitica_media"
 status_week = "Resumen de la semana política en twitter #DataPolitica_semana"
 
-status_wc_pp = "Qué dice la gente sobre el #PP: " + '%s/%s/%s'% ( ystday.day, ystday.month, ystday.year)
-status_wc_podemos = "Qué dice la gente sobre #Podemos: "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_wc_psoe = "Qué dice la gente sobre el #PSOE: "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_wc_cs = "Qué dice la gente sobre #Ciudadanos: "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_wc_iu = "Qué dice la gente sobre #IU: " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-status_wc_upyd = "Qué dice la gente sobre #UPyD: " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
+# Statuses of top concepts
+status_concepts_top_pp      = status_concepts_top[0:len(status_concepts_top[0])-1]
+status_concepts_top_podemos = status_concepts_top[1:len(status_concepts_top[1])-1]
+status_concepts_top_psoe    = status_concepts_top[2:len(status_concepts_top[2])-1]
+status_concepts_top_cs      = status_concepts_top[3:len(status_concepts_top[3])-1]
+status_concepts_top_iu      = status_concepts_top[4:len(status_concepts_top[4])-1]
+status_concepts_top_upyd    = status_concepts_top[5:len(status_concepts_top[5])-1]
 
 # Paths to status
 status_top_pp      = status_top[0][0:len(status_top[0])-1]
@@ -76,31 +82,25 @@ status_top_cs      = status_top[3][0:len(status_top[3])-1]
 status_top_iu      = status_top[4][0:len(status_top[4])-1]
 status_top_upyd    = status_top[5][0:len(status_top[5])-1]
 
-#status_top_pp = "Top 10 tuiteros del #PP #DataPolitica_TopPP " + '%s/%s/%s'% ( ystday.day, ystday.month, ystday.year)
-#status_top_podemos ="Top 10 tuiteros de #Podemos #DataPolitica_TopPodemos "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-#status_top_psoe = "Top 10 tuiteros del #PSOE #DataPolitica_TopPSOE "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-#status_top_cs = "Top 10 tuiteros de #Ciudadanos #DataPolitica_TopCs "+'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-#status_top_iu = "Top 10 tuiteros de #IU #DataPolitica_TopIU " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-#status_top_upyd ="Top 10 tuiteros de #UPyD #DataPolitica_TopUPyD " +'%s/%s/%s' % ( ystday.day, ystday.month, ystday.year)
-
-status_inter = "Interacciones entre tweets " +'%s/%s/%s'% ( ystday.day, ystday.month, ystday.year) + " #DataPolitica_Interacciones"
+status_inter = "Interacciones entre tweets: " + date_ystday + " #DataPolitica_Interacciones"
 
 
 # Update
-api.update_with_media(path_wc_pp       , status = status_wc_pp)
-api.update_with_media(path_wc_podemos  , status = status_wc_podemos)
-api.update_with_media(path_wc_psoe     , status = status_wc_psoe)
-api.update_with_media(path_wc_cs       , status = status_wc_cs)
-api.update_with_media(path_wc_iu       , status = status_wc_iu)
-api.update_with_media(path_wc_upyd     , status = status_wc_upyd)
+# Wordclouds
+api.update_with_media(path_wc_pp       , status = status_concepts_top_pp)
+api.update_with_media(path_wc_podemos  , status = status_concepts_top_podemos)
+api.update_with_media(path_wc_psoe     , status = status_concepts_top_psoe)
+api.update_with_media(path_wc_cs       , status = status_concepts_top_cs)
+api.update_with_media(path_wc_iu       , status = status_concepts_top_iu)
+api.update_with_media(path_wc_upyd     , status = status_concepts_top_upyd)
 
+# Top tweeters
 api.update_with_media(path_top_pp      , status = status_top_pp)
 api.update_with_media(path_top_podemos , status = status_top_podemos)
 api.update_with_media(path_top_psoe    , status = status_top_psoe)
 api.update_with_media(path_top_cs      , status = status_top_cs)
 api.update_with_media(path_top_iu      , status = status_top_iu)
 api.update_with_media(path_top_upyd    , status = status_top_upyd)
-
 
 api.update_with_media(path_mean , status = status_mean )
 api.update_with_media(path_all  , status = status_all  )
@@ -111,5 +111,7 @@ if len(paths)==17:
     api.update_with_media(path_week, status = status_week)
 
 
-f.close()
+# Close connections
+f1.close()
 f2.close()
+f3.close()
